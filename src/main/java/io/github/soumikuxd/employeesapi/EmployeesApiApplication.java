64fe0b4a59a194be764f194c06df220658bd5e9e
Soumik <soumik.uxd@gmail.com>
2021-05-16 00:57:22 +0200

    Swagger init

--- src/main/java/io/github/soumikuxd/employeesapi/EmployeesApiApplication.java
+++ src/main/java/io/github/soumikuxd/employeesapi/EmployeesApiApplication.java
@@ -2,12 +2,12 @@ package io.github.soumikuxd.employeesapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EmployeesApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApiApplication.class, args);
 	}
-
 }
